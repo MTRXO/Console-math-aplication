be77d71e48f814060845c9e6248b3db9a6988a6e
Mateusz Drążkiewicz <matekudr@gmail.com>
2023-03-07 22:10:07 +0100

    menu navigation

--- Console math aplication/Program.cs
+++ Console math aplication/Program.cs
@@ -9,8 +9,35 @@ namespace AppBody
             Console.WriteLine("1.Start");
             Console.WriteLine("2.Exit");
 			Console.WriteLine("Choose option:");
-			
-			
+            string choose1 = Console.ReadLine();
+
+			if (choose1 == "1")
+			{
+				Console.Clear();
+				Console.WriteLine("Choose topic:");
+                Console.WriteLine("1.Figure fields");
+                Console.WriteLine("2.Quadratic function");
+				
+
+					
+
+
+            }
+            else if (choose1 == "2")
+            {
+                Console.Clear();
+				Console.WriteLine("You closed this app");
+            }
+			else
+			{
+				Console.Clear();
+				Console.WriteLine("Wrong input, try again");
+			}
+
+		
+			Console.ReadLine();
+
+
 
         }
 
