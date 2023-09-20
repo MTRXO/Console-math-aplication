41834fc57589f978f8dbc082d21c6eb588a02d1e
Mateusz <matekudr@gmail.com>
2023-09-20 10:56:14 +0200

    Create unit tests class and speparate whole methods to other class

--- Console math aplication/Program.cs
+++ Console math aplication/Program.cs
@@ -3,10 +3,12 @@ using System.Text.RegularExpressions;
 
 namespace AppBody
 {
-    class Program
+    public class Program
     {
+       
         static void Main(string[] args) //This is a main display function
         {
+            Calculations calculations = new Calculations();
             Console.WriteLine("GENERAL MATH CALCULATOR");
             Console.WriteLine("1.Start");
             Console.WriteLine("2.Exit");
@@ -32,17 +34,17 @@ namespace AppBody
 
                     switch (choose3) // Another types of figure fields
                     {
-                        case "1": 
+                        case "1":
                             Console.Clear();
                             Console.WriteLine("Square");
                             Console.WriteLine("Write below length of first side: ");
-                            string value1= Console.ReadLine();     
-                            Console.WriteLine( "Field =  " + SquareField(Convert.ToDouble(value1)));
+                            string value1 = Console.ReadLine();
+                            Console.WriteLine("Field =  " + calculations.SquareField(Convert.ToDouble(value1)));
                             Console.ReadLine();
-                       
+
                             break;
 
-                        case "2":   
+                        case "2":
                             Console.Clear();
                             Console.WriteLine("Rectangle");
                             Console.WriteLine("Write below leanght of both sides");
@@ -50,7 +52,7 @@ namespace AppBody
                             string value2 = Console.ReadLine();
                             Console.WriteLine("b:");
                             string value3 = Console.ReadLine();
-                            Console.WriteLine( "Field: " + RectangleField(Convert.ToDouble(value2), Convert.ToDouble(value3)));
+                            Console.WriteLine("Field: " + calculations.RectangleField(Convert.ToDouble(value2), Convert.ToDouble(value3)));
                             Console.ReadLine();
                             break;
 
@@ -62,12 +64,12 @@ namespace AppBody
                             string value4 = Console.ReadLine();
                             Console.WriteLine("h:");
                             string value5 = Console.ReadLine();
-                            Console.WriteLine("Field: " + RectangularTriangleFiled(Convert.ToDouble(value4),Convert.ToDouble(value5)));
+                            Console.WriteLine("Field: " + calculations.RectangularTriangleFiled(Convert.ToDouble(value4), Convert.ToDouble(value5)));
                             Console.ReadLine();
-   
-                            break;  
+
+                            break;
                     }
-                  
+
 
                 }
 
@@ -81,32 +83,32 @@ namespace AppBody
                     string b = Console.ReadLine();
                     Console.WriteLine("c:");
                     string c = Console.ReadLine();
-                 
+
                     Console.Clear();
-                    double Delt = Delta(Convert.ToDouble(a), Convert.ToDouble(b), Convert.ToDouble(c));
-                    Console.WriteLine("Delta: "+Delt);
-                    Console.WriteLine(IsZeroPlaces(Delt));
+                    double Delt = calculations.Delta(Convert.ToDouble(a), Convert.ToDouble(b), Convert.ToDouble(c));
+                    Console.WriteLine("Delta: " + Delt);
+                    Console.WriteLine(calculations.IsZeroPlaces(Delt));
 
                     if (Delt > 0) // Here program decides what if delta have specified value
                     {
                         Console.WriteLine("Value of zero places:");
-                        Console.WriteLine(ZeroPlacesCounterx1(Convert.ToDouble(a), Convert.ToDouble(b), Delt));
-                        Console.WriteLine(ZeroPlacesCounterx2(Convert.ToDouble(a), Convert.ToDouble(b), Delt));
-                        
+                        Console.WriteLine(calculations.ZeroPlacesCounterx1(Convert.ToDouble(a), Convert.ToDouble(b), Delt));
+                        Console.WriteLine(calculations.ZeroPlacesCounterx2(Convert.ToDouble(a), Convert.ToDouble(b), Delt));
+
 
                     }
                     else if (Delt < 0)
                     {
                         Console.WriteLine("This function have no zero places");
-                        
-                        
+
+
 
                     }
                     else if (Delt == 0)
                     {
                         Console.WriteLine("This function have one zero place");
-                        Console.WriteLine(OneZeroPlace(Convert.ToDouble(a), Convert.ToDouble(b)));
-                       
+                        Console.WriteLine(calculations.OneZeroPlace(Convert.ToDouble(a), Convert.ToDouble(b)));
+
 
 
                     }
@@ -135,7 +137,13 @@ namespace AppBody
 
         }
 
-        static double Delta(double a, double b, double c) //this function count delta value
+    }
+
+
+
+    public class Calculations
+    {
+        public double Delta(double a, double b, double c) //this function count delta value
         {
 
             double del = (b * b) - (4 * a * c);
@@ -144,7 +152,7 @@ namespace AppBody
         }
 
 
-        static string IsZeroPlaces(double d) //this function check if delta have any zero places
+        public string IsZeroPlaces(double d) //this function check if delta have any zero places
         {
             if (d > 0)
             {
@@ -162,43 +170,45 @@ namespace AppBody
             else return "wrong input";
 
         }
-        static double ZeroPlacesCounterx1(double a, double b, double delta) //This function count first zero place
+        public double ZeroPlacesCounterx1(double a, double b, double delta) //This function count first zero place
         {
             double x1 = (-b - Math.Sqrt(delta));
             return x1 / (2 * a);
 
         }
-        static double ZeroPlacesCounterx2(double a, double b, double delta) //This function count second zero place 
+        public double ZeroPlacesCounterx2(double a, double b, double delta) //This function count second zero place 
         {
             double x2 = (-b + Math.Sqrt(delta));
             return x2 / (2 * a);
 
         }
-        static double OneZeroPlace(double a, double b) //This function count zero place in case when delta == 0 
+        public double OneZeroPlace(double a, double b) //This function count zero place in case when delta == 0 
         {
             double x0 = -b / (2 * a);
             return x0;
         }
-        static double SquareField(double a) //This function count square field 
+        public double SquareField(double a) //This function count square field 
         {
             return a * a;
         }
-        static double RectangleField(double a, double b) //This function count rectangle field
+        public double RectangleField(double a, double b) //This function count rectangle field
         {
             return a * b;
         }
-        static double RectangularTriangleFiled(double a,double h) //This function count  Rectangular triangle filed
+        public double RectangularTriangleFiled(double a, double h) //This function count  Rectangular triangle filed
         {
-              return (a * h) / 2;
-           
+            return (a * h) / 2;
+
         }
-
-
-
-
-
-
-
     }
+    
+
+
+
+
+
+
+
+    
 
 }
