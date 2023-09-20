f2bf50a047e335f6300d4cb1127ef8b17c27057d
Mateusz <matekudr@gmail.com>
2023-09-20 13:52:42 +0200

    write the most important tests

--- Console_Math_Aplication_Tests/ProgramClassTests.cs
+++ Console_Math_Aplication_Tests/ProgramClassTests.cs
@@ -1,23 +1,55 @@
-﻿using AppBody;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using AppBody;
 
 namespace Console_Math_Aplication_Tests
 {
     internal class ProgramClassTests
     {
-        [Test]
+        Calculations calculations = new Calculations();
 
+        [Test]
         public void RectangularTriangleFiled_Test()
         {
-            Calculations calculations = new Calculations();
             double field = calculations.RectangularTriangleFiled(2, 4);
-            double RightField = 4;
-            Assert.AreEqual(RightField, field);
+            double CorrectField = 4;
+            Assert.AreEqual(CorrectField, field);   
+        }
+        [Test]
+        public void RectangleField_Test()
+        {
+            double field = calculations.RectangleField(2, 4);
+            double CorrectField = 8;
+            Assert.AreEqual(CorrectField, field);
+        }
+        [Test]
+        public void SquareField_Test() 
+        {
+            double field = calculations.SquareField(10);
+            double CorrectField = 100;
+            Assert.AreEqual(CorrectField, field);
             
+
+        }
+        [Test]
+        public void OneZeroPlace_Test() 
+        {
+            double ZeroPlaceValue = calculations.OneZeroPlace(5, 10);
+            double CorrectZeroPlaceValue = -1;
+            Assert.AreEqual(CorrectZeroPlaceValue, ZeroPlaceValue);   
+        }
+        [Test]
+        public void ZeroPlacesCounterx2_Test()
+        {
+            double ZeroPlacex2 = calculations.ZeroPlacesCounterx2(5,-5, 25);
+            double CorrectZeroPlacex2 = 1;
+            Assert.AreEqual(CorrectZeroPlacex2, ZeroPlacex2);
+        }
+        [Test]
+        public void ZeroPlacesCounterx1_Test()
+        {
+            double ZeroPlacex1 = calculations.ZeroPlacesCounterx2(5, -5, 25);
+            double CorrectZeroPlacex1 = 1;
+            Assert.AreEqual(CorrectZeroPlacex1, ZeroPlacex1);
         }
 
     }
