662bd40386db15eb5e0f696361e89283766395af
Mateusz <matekudr@gmail.com>
2023-09-20 11:10:30 +0200

    write first test

--- Console_Math_Aplication_Tests/ProgramClassTests.cs
+++ Console_Math_Aplication_Tests/ProgramClassTests.cs
@@ -9,6 +9,16 @@ namespace Console_Math_Aplication_Tests
 {
     internal class ProgramClassTests
     {
+        [Test]
+
+        public void RectangularTriangleFiled_Test()
+        {
+            Calculations calculations = new Calculations();
+            double field = calculations.RectangularTriangleFiled(2, 4);
+            double RightField = 4;
+            Assert.AreEqual(RightField, field);
+            
+        }
 
     }
 }
