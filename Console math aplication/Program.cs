4fb4483bb6843aecb8fe79fff72022c34c7b38c7
Mateusz <matekudr@gmail.com>
2023-10-26 17:48:53 +0200

    add loop while

--- Console math aplication/Program.cs
+++ Console math aplication/Program.cs
@@ -5,115 +5,131 @@ namespace AppBody
 {
     public class Program
     {
-       
+
         static void Main(string[] args) //This is a main display function
         {
-            Calculations calculations = new Calculations();
-            Console.WriteLine("GENERAL MATH CALCULATOR");
-            Console.WriteLine("1.Start");
-            Console.WriteLine("2.Exit");
-            Console.WriteLine("Choose option:");
-            string choose1 = Console.ReadLine();
-
-            if (choose1 == "1") // What will happen if u choose any option
+            while (true)
             {
-                Console.Clear();
-                Console.WriteLine("Choose topic:");
-                Console.WriteLine("1.Figure fields");
-                Console.WriteLine("2.Quadratic function");
-                string choose2 = Console.ReadLine();
+                Calculations calculations = new Calculations();
+                Console.WriteLine("GENERAL MATH CALCULATOR");
+                Console.WriteLine("1.Start");
+                Console.WriteLine("2.Exit");
+                Console.WriteLine("Choose option:");
+                string choose1 = Console.ReadLine();
 
-                if (choose2 == "1") // when user choose firugre fields
+                if (choose1 == "1") // What will happen if u choose any option
                 {
                     Console.Clear();
-                    Console.WriteLine("Choose figure");
-                    Console.WriteLine("1.Square");
-                    Console.WriteLine("2.Rectangle");
-                    Console.WriteLine("3.Rectangular triangle");
-                    string choose3 = Console.ReadLine();
+                    Console.WriteLine("Choose topic:");
+                    Console.WriteLine("1.Figure fields");
+                    Console.WriteLine("2.Quadratic function");
+                    string choose2 = Console.ReadLine();
 
-                    switch (choose3) // Another types of figure fields
+                    if (choose2 == "1") // when user choose firugre fields
                     {
-                        case "1":
-                            Console.Clear();
-                            Console.WriteLine("Square");
-                            Console.WriteLine("Write below length of first side: ");
-                            string value1 = Console.ReadLine();
-                            Console.WriteLine("Field =  " + calculations.SquareField(Convert.ToDouble(value1)));
-                            Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("Choose figure");
+                        Console.WriteLine("1.Square");
+                        Console.WriteLine("2.Rectangle");
+                        Console.WriteLine("3.Rectangular triangle");
+                        string choose3 = Console.ReadLine();
 
-                            break;
+                        switch (choose3) // Another types of figure fields
+                        {
+                            case "1":
+                                Console.Clear();
+                                Console.WriteLine("Square");
+                                Console.WriteLine("Write below length of first side: ");
+                                string value1 = Console.ReadLine();
+                                Console.WriteLine("Field =  " + calculations.SquareField(Convert.ToDouble(value1)));
+                                Console.ReadLine();
 
-                        case "2":
-                            Console.Clear();
-                            Console.WriteLine("Rectangle");
-                            Console.WriteLine("Write below leanght of both sides");
-                            Console.WriteLine("a:");
-                            string value2 = Console.ReadLine();
-                            Console.WriteLine("b:");
-                            string value3 = Console.ReadLine();
-                            Console.WriteLine("Field: " + calculations.RectangleField(Convert.ToDouble(value2), Convert.ToDouble(value3)));
-                            Console.ReadLine();
-                            break;
+                                break;
 
-                        case "3":
-                            Console.Clear();
-                            Console.WriteLine("Rectangular triangle");
-                            Console.WriteLine("Write below a and h ");
-                            Console.WriteLine("a:");
-                            string value4 = Console.ReadLine();
-                            Console.WriteLine("h:");
-                            string value5 = Console.ReadLine();
-                            Console.WriteLine("Field: " + calculations.RectangularTriangleFiled(Convert.ToDouble(value4), Convert.ToDouble(value5)));
-                            Console.ReadLine();
+                            case "2":
+                                Console.Clear();
+                                Console.WriteLine("Rectangle");
+                                Console.WriteLine("Write below leanght of both sides");
+                                Console.WriteLine("a:");
+                                string value2 = Console.ReadLine();
+                                Console.WriteLine("b:");
+                                string value3 = Console.ReadLine();
+                                Console.WriteLine("Field: " + calculations.RectangleField(Convert.ToDouble(value2), Convert.ToDouble(value3)));
+                                Console.ReadLine();
+                                break;
 
-                            break;
+                            case "3":
+                                Console.Clear();
+                                Console.WriteLine("Rectangular triangle");
+                                Console.WriteLine("Write below a and h ");
+                                Console.WriteLine("a:");
+                                string value4 = Console.ReadLine();
+                                Console.WriteLine("h:");
+                                string value5 = Console.ReadLine();
+                                Console.WriteLine("Field: " + calculations.RectangularTriangleFiled(Convert.ToDouble(value4), Convert.ToDouble(value5)));
+                                Console.ReadLine();
+
+                                break;
+                        }
+
+
                     }
+
+                    else if (choose2 == "2") //if user choosed a quadratic fucntion
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Write below a,b and c and I'll count delta and zero places for you ");
+                        Console.WriteLine("a:");
+                        string a = Console.ReadLine();
+                        Console.WriteLine("b:");
+                        string b = Console.ReadLine();
+                        Console.WriteLine("c:");
+                        string c = Console.ReadLine();
+
+                        Console.Clear();
+                        double Delt = calculations.Delta(Convert.ToDouble(a), Convert.ToDouble(b), Convert.ToDouble(c));
+                        Console.WriteLine("Delta: " + Delt);
+                        Console.WriteLine(calculations.IsZeroPlaces(Delt));
+
+                        if (Delt > 0) // Here program decides what if delta have specified value
+                        {
+                            Console.WriteLine("Value of zero places:");
+                            Console.WriteLine(calculations.ZeroPlacesCounterx1(Convert.ToDouble(a), Convert.ToDouble(b), Delt));
+                            Console.WriteLine(calculations.ZeroPlacesCounterx2(Convert.ToDouble(a), Convert.ToDouble(b), Delt));
+
+
+                        }
+                        else if (Delt < 0)
+                        {
+                            Console.WriteLine("This function have no zero places");
+
+
+
+                        }
+                        else if (Delt == 0)
+                        {
+                            Console.WriteLine("This function have one zero place");
+                            Console.WriteLine(calculations.OneZeroPlace(Convert.ToDouble(a), Convert.ToDouble(b)));
+
+
+
+                        }
+                        Console.ReadLine();
+
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Wrong input, try again");
+                    }
+
 
 
                 }
-
-                else if (choose2 == "2") //if user choosed a quadratic fucntion
+                else if (choose1 == "2")
                 {
                     Console.Clear();
-                    Console.WriteLine("Write below a,b and c and I'll count delta and zero places for you ");
-                    Console.WriteLine("a:");
-                    string a = Console.ReadLine();
-                    Console.WriteLine("b:");
-                    string b = Console.ReadLine();
-                    Console.WriteLine("c:");
-                    string c = Console.ReadLine();
-
-                    Console.Clear();
-                    double Delt = calculations.Delta(Convert.ToDouble(a), Convert.ToDouble(b), Convert.ToDouble(c));
-                    Console.WriteLine("Delta: " + Delt);
-                    Console.WriteLine(calculations.IsZeroPlaces(Delt));
-
-                    if (Delt > 0) // Here program decides what if delta have specified value
-                    {
-                        Console.WriteLine("Value of zero places:");
-                        Console.WriteLine(calculations.ZeroPlacesCounterx1(Convert.ToDouble(a), Convert.ToDouble(b), Delt));
-                        Console.WriteLine(calculations.ZeroPlacesCounterx2(Convert.ToDouble(a), Convert.ToDouble(b), Delt));
-
-
-                    }
-                    else if (Delt < 0)
-                    {
-                        Console.WriteLine("This function have no zero places");
-
-
-
-                    }
-                    else if (Delt == 0)
-                    {
-                        Console.WriteLine("This function have one zero place");
-                        Console.WriteLine(calculations.OneZeroPlace(Convert.ToDouble(a), Convert.ToDouble(b)));
-
-
-
-                    }
-                    Console.ReadLine();
-
+                    Console.WriteLine("You closed this app");
                 }
                 else
                 {
@@ -121,24 +137,10 @@ namespace AppBody
                     Console.WriteLine("Wrong input, try again");
                 }
 
-
-
-            }
-            else if (choose1 == "2")
-            {
-                Console.Clear();
-                Console.WriteLine("You closed this app");
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Wrong input, try again");
             }
 
         }
-
     }
-
 
 
     public class Calculations
