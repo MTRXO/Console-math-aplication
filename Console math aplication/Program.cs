c4498ad1b5f70e1939b52ef5365a0bb8b3324ce5
Mateusz <matekudr@gmail.com>
2023-10-27 12:42:10 +0200

    add end of loop

--- Console math aplication/Program.cs
+++ Console math aplication/Program.cs
@@ -130,6 +130,8 @@ namespace AppBody
                 {
                     Console.Clear();
                     Console.WriteLine("You closed this app");
+                    break;
+                    
                 }
                 else
                 {
