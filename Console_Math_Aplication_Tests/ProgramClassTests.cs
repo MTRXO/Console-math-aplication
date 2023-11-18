e1c89ffb7757d95d5e2cd648da1bd2cf7bb2add0
Mateusz <matekudr@gmail.com>
2023-11-18 14:12:48 +0100

    write simple test to UnitConverterWeight

--- Console_Math_Aplication_Tests/ProgramClassTests.cs
+++ Console_Math_Aplication_Tests/ProgramClassTests.cs
@@ -52,12 +52,19 @@ namespace Console_Math_Aplication_Tests
             Assert.AreEqual(CorrectZeroPlacex1, ZeroPlacex1);
         }
         [Test]
-        public void UnitCoverter_test()
+        public void UnitCoverterLenght_test()
         {
             string[] BaseMilimeters = calculations.UnitConverterLenght(1, 10);
             string[] CorrectValue = { "Centimetres: 1", "Meters: 10000 ", "Milimeters:10" };
             Assert.AreEqual(CorrectValue, BaseMilimeters);
 
+        }
+        [Test]
+        public void UnitCoverterWeight_test()
+        {
+            string[] BaseMilimeters = calculations.UnitConverterWeight(1, 10);
+            string[] CorrectValue = { "Grams: 10", "Kilograms: 0.010 ", "Tons:0.0010" };
+            Assert.AreEqual(CorrectValue, BaseMilimeters);
         }
 
 
